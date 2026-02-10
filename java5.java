b599e92f5b1570111b2ede16687a6a2d8097f6f3
JEEVAN-PS <jeevanguruprasad@gmail.com>
2026-02-10 12:05:20 +0530

    Square number

---
+++ java5.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class SquareCheck {
+
+    // Function to calculate square of distance
+    static int distSq(Point a, Point b) {
+        return (a.x - b.x) * (a.x - b.x) +
+               (a.y - b.y) * (a.y - b.y);
+    }
+
+    // Function to check square
+    static boolean isSquare(Point p1, Point p2, Point p3, Point p4) {
+
+        int[] d = new int[6];
+
+        d[0] = distSq(p1, p2);
+        d[1] = distSq(p1, p3);
+        d[2] = distSq(p1, p4);
+        d[3] = distSq(p2, p3);
+        d[4] = distSq(p2, p4);
+        d[5] = distSq(p3, p4);
+
+        Arrays.sort(d);
+
+        // First 4 distances should be same (sides)
+        // Last 2 should be same (diagonals)
+        return d[0] > 0 &&
+               d[0] == d[1] &&
+               d[1] == d[2] &&
+               d[2] == d[3] &&
+               d[4] == d[5];
+    }
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(20, 10);
+        Point p2 = new Point(10, 20);
+        Point p3 = new Point(20, 20);
+        Point p4 = new Point(10, 10);
+
+        if (isSquare(p1, p2, p3, p4))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
