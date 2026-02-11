18c29c6ce6b6c2c64ed20e15789a31b543839d79
JEEVAN-PS <jeevanguruprasad@gmail.com>
2026-02-11 10:26:28 +0530

    Add files via upload

---
+++ java6.java
@@ -0,0 +1,28 @@
+
+class Student {
+
+    String name;
+    int age;
+    float cgpa;
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    
+        Student s1 = new Student();
+
+
+        s1.name = "Jeevan";
+        s1.age = 21;
+        s1.cgpa = 8.3;
+        s1.display();
+    }
+}
